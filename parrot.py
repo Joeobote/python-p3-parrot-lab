0cd0bed966b1690146bd0b886f74de0f14fea55a
JosephObote <joeobote01@gmail.com>
2023-09-18 09:34:18 +0300

    completed assignment

---
+++ parrot.py
@@ -0,0 +1,9 @@
+def parrot(input_string="Squack!"):
+    print(input_string)
+    return input_string
+
+result_default = parrot()  
+
+message = "Hello, I'm a parrot!"
+result_custom = parrot(message)  
+
